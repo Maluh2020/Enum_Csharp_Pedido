56780ecbc6472f1354156cb50f62611b49cad6d1
Maluh2020 <maluhmyth@gmail.com>
2020-11-14 20:20:00 -0300

    first commit

---
+++ Enum_Pedido_Cliente/Entities/OrderItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Enum_Pedido_Cliente.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, double price, Product produto)
+        {
+            Quantity = quantity;
+            Price = price;
+            Product = produto;
+        }
+
+        public double SubTotal()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return Product.Name
+                + ", $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Quantity: "
+                + Quantity
+                + ", SubTotal: $"
+                + SubTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
